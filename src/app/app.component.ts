2f6c1e447f26a3666f893c5dad1a7654e36a42b8
qhernandez927 <quinnh2791@gmail.com>
2019-12-29 21:10:34 -0600

    For loop for data table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,8 @@ import { Satellite } from './satellite';
 })
 export class AppComponent {
   title = 'orbit-report';
-}
+  sourceList: Satellite[];
 
-let sourceList: Satellite[]; [{
   constructor() {
     this.sourceList = [
       new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
@@ -20,6 +19,8 @@ let sourceList: Satellite[]; [{
       new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
     ];
   }
-}];
+}
+
+
 
 
